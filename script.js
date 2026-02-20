74cad1084f94a3ba3c1373f24978a4fdf9b31d9b
ArtK-web <artem123954452@gmail.com>
2026-02-20 15:48:21 +0200

    Долбавлена вторая кнопка и картинка для интерактивности

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 document.getElementById('helloBtn').onclick = function() {
     document.getElementById('message').textContent = 'Привет! Всё работает!';
 };
+// Первая кнопка
+document.getElementById('helloBtn').onclick = function() {
+    document.getElementById('message').textContent = 'Привет! Всё работает!';
+};
+
+// Вторая кнопка
+document.getElementById('changeImgBtn').onclick = function() {
+    document.getElementById('myImage').src = 'https://picsum.photos/300/200'; //https://agro-business.com.ua/images/756188213182894_1.jpg
+};
